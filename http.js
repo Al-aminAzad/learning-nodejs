875fc754e15373927ce355045546585f9a9d171d
Al-aminAzad <alaminazad240@gmail.com>
2021-01-22 19:26:33 +0600

    http module and fs module combined

--- http.js
+++ http.js
@@ -1,15 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.write("Hello programmers!");
-    res.write("How are you");
-    res.end();
+    fs.readFile("./index.html", (err, data) => {
+      res.write(data);
+      res.write("Hello programmers!");
+      res.write("How are you");
+      res.end();
+    });
   } else if (req.url === "/about") {
     res.write("This is about page");
     res.end();
-  }else{
-      res.write('Not found');
-      res.end();
+  } else {
+    res.write("Not found");
+    res.end();
   }
 });
 // server.on('connection',()=>{
